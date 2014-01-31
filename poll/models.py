26be4baf31d0f1c0e8e248c2f0dc7f7ca01faba9
Luan <luanpab@gmail.com>
2014-01-31 15:19:51 -0200

    Using get_user_model to support custom users

--- poll/models.py
+++ poll/models.py
@@ -3,7 +3,7 @@
 import datetime
 from django.db import models
 from django.utils.translation import gettext as _
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.db.models.manager import Manager
 from django.core.exceptions import ValidationError
 
@@ -16,7 +16,7 @@ class Poll(models.Model):
     title = models.CharField(max_length=250, verbose_name=_('question'))
     date = models.DateField(verbose_name=_('date'), default=datetime.date.today)
     is_published = models.BooleanField(default=True, verbose_name=_('is published'))
-    
+
     objects = models.Manager()
     published = PublishedManager()
 
@@ -27,25 +27,25 @@ class Poll(models.Model):
 
     def __unicode__(self):
         return self.title
-        
+
     def get_vote_count(self):
         return Vote.objects.filter(poll=self).count()
     vote_count = property(fget=get_vote_count)
-    
+
     def get_cookie_name(self):
-        return str('poll_%s' % (self.pk)) 
-    
+        return str('poll_%s' % (self.pk))
+
 
 class Item(models.Model):
     poll = models.ForeignKey(Poll)
     value = models.CharField(max_length=250, verbose_name=_('value'))
     pos = models.SmallIntegerField(default='0', verbose_name=_('position'))
-    
+
     class Meta:
         verbose_name = _('answer')
         verbose_name_plural = _('answers')
         ordering = ['pos']
-    
+
     def __unicode__(self):
         return u'%s' % (self.value,)
 
@@ -58,15 +58,16 @@ class Vote(models.Model):
     poll = models.ForeignKey(Poll, verbose_name=_('poll'))
     item = models.ForeignKey(Item, verbose_name=_('voted item'))
     ip = models.IPAddressField(verbose_name=_('user\'s IP'))
-    user = models.ForeignKey(User, blank=True, null=True, verbose_name=_('user'))
+    user = models.ForeignKey(get_user_model(), blank=True, null=True,
+                             verbose_name=_('user'))
     datetime = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         verbose_name = _('vote')
         verbose_name_plural = _('votes')
-    
+
     def __unicode__(self):
-        if isinstance(self.user, User):
-            return self.user.username
+        user = get_user_model()
+        if isinstance(self.user, user):
+            return getattr(user, user.USERNAME_FIELD)
         return self.ip
-    
